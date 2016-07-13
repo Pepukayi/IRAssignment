f7fa4d77c34f579ee4d5d8c7de2a981872a800e4
Mumanyik1 <chitondop@cput.ac.za>
2016-07-13 17:28:25 +0200

    Initial commit

---
+++ src/main/scala/com/Raduis/MyRouteMain.scala
@@ -0,0 +1,47 @@
+package com.Raduis
+
+/**
+ * A simple scala application that displays the top 10 Hashtags on twiiter page.
+ */
+
+import com.danielasfregola.twitter4s.TwitterClient
+import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
+import com.danielasfregola.twitter4s.entities.{HashTag, Tweet}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object MyRouteMain  {
+
+  val Accesstoken = "190575331-J5SJ7Tc9YBZnbuRzP6Stvjw0xvJwTO4ADIwaNU6M"
+  val AccessSecret = "2E9qL3ArhnxU5r1mxU6lTqLRPrXaK40A4qYhz5hV7cz8C"
+  val ConsumerKey = "fiXrjWdCPdYl7IqqSag3tjkLG"
+  val ConsumerSecret = "7JTD51VHxt3hV7OtyOe7Jfd0sXLrXsm3MnwpmR5pEwULSItY4S"
+
+  def main(args: Array[String]) {
+
+    println("Okay")
+
+    val consumerToken = ConsumerToken(key = ConsumerKey, secret = ConsumerSecret)
+    val accessToken = AccessToken(key = Accesstoken, secret = AccessSecret)
+    val client = new TwitterClient(consumerToken, accessToken)
+
+    def getTopHashtags(tweets: Seq[Tweet], n: Int = 10): Seq[(String, Int)] = {
+      val hashtags: Seq[Seq[HashTag]] = tweets.map { tweet =>
+        tweet.entities.map(_.hashtags).getOrElse(Seq.empty)
+      }
+      val hashtagTexts: Seq[String] = hashtags.flatten.map(_.text.toLowerCase)
+      val hashtagFrequencies: Map[String, Int] = hashtagTexts.groupBy(identity).mapValues(_.size)
+      hashtagFrequencies.toSeq.sortBy { case (entity, frequency) => -frequency }.take(n)
+    }
+
+    val user = "F1"
+
+    client.getUserTimelineForUser(screen_name = user, count = 200).map { tweets =>
+      val topHashtags: Seq[((String, Int), Int)] = getTopHashtags(tweets).zipWithIndex
+      val rankings = topHashtags.map { case ((entity, frequency), idx) => s"[${idx + 1}] $entity (found $frequency times)" }
+      println(s"${user.toUpperCase}'S TOP HASHTAGS:")
+      println(rankings.mkString("\n"))
+
+    }
+  }
+}
+
