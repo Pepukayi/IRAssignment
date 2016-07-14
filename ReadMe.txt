ed31853e2e4f84685ecaf10f775d74bb3a55a85b
Mumanyik1 <chitondop@cput.ac.za>
2016-07-14 15:20:31 +0200

    Updated Name of Project

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,15 +1,24 @@
-Camel Router with Scala DSL Project
+Twitter Client To Analyse Top Hashtags of a Twitter Page
 ===================================
 
-To build this project use
 
-    mvn install
+SetUp:
+
+Register an application at http://apps.twitter.com
+
+Replace the static variables in the companion object with consumer key,
+consumer secret, access key and access secret generated when registering the app.
+
+To build this project use:
+
+Scala version 2.11.*
+Java 8
+Load dependancies as desribed in the pom.xml
 
 To run this project
+You will be prompted to enter a valid Twitter page id or name. You then press enter and the
+top 10 Hashtags mentioned on that page will be outputed to the console as well as to a file
+in your project directory with the name of the page in concern.
 
-    mvn exec:java
-    
-For more help see the Apache Camel documentation
 
-    http://camel.apache.org/
 
--- src/main/scala/com/Raduis/MyRouteMain.scala
+++ src/main/scala/com/Raduis/MyRouteMain.scala
@@ -4,10 +4,18 @@ package com.Raduis
  * A simple scala application that displays the top 10 Hashtags on twiiter page.
  */
 
+import java.io._
+
 import com.danielasfregola.twitter4s.TwitterClient
 import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
 import com.danielasfregola.twitter4s.entities.{HashTag, Tweet}
+
+import scala.io.StdIn._
 import scala.concurrent.ExecutionContext.Implicits.global
+
+
+
+
 
 object MyRouteMain  {
 
@@ -24,16 +32,7 @@ object MyRouteMain  {
     val accessToken = AccessToken(key = Accesstoken, secret = AccessSecret)
     val client = new TwitterClient(consumerToken, accessToken)
 
-    def getTopHashtags(tweets: Seq[Tweet], n: Int = 10): Seq[(String, Int)] = {
-      val hashtags: Seq[Seq[HashTag]] = tweets.map { tweet =>
-        tweet.entities.map(_.hashtags).getOrElse(Seq.empty)
-      }
-      val hashtagTexts: Seq[String] = hashtags.flatten.map(_.text.toLowerCase)
-      val hashtagFrequencies: Map[String, Int] = hashtagTexts.groupBy(identity).mapValues(_.size)
-      hashtagFrequencies.toSeq.sortBy { case (entity, frequency) => -frequency }.take(n)
-    }
-
-    val user = "F1"
+    val user = readLine("Please enter the user name or ID of the twitter page you would like to analyse Hashtags tweets from: ".toUpperCase)
 
     client.getUserTimelineForUser(screen_name = user, count = 200).map { tweets =>
       val topHashtags: Seq[((String, Int), Int)] = getTopHashtags(tweets).zipWithIndex
@@ -41,7 +40,22 @@ object MyRouteMain  {
       println(s"${user.toUpperCase}'S TOP HASHTAGS:")
       println(rankings.mkString("\n"))
 
+      rankings.foreach{println}
+
+      val writer = new PrintWriter(new File(user + "s_top_hash_tags.txt"))
+      writer.write(rankings.mkString("\n"))
+      writer.close()
     }
+  }
+
+  def getTopHashtags(tweets: Seq[Tweet], n: Int = 10): Seq[(String, Int)] =
+  {
+    val hashtags: Seq[Seq[HashTag]] = tweets.map { tweet =>
+      tweet.entities.map(_.hashtags).getOrElse(Seq.empty)
+    }
+    val hashtagTexts: Seq[String] = hashtags.flatten.map(_.text.toLowerCase)
+    val hashtagFrequencies: Map[String, Int] = hashtagTexts.groupBy(identity).mapValues(_.size)
+    hashtagFrequencies.toSeq.sortBy { case (entity, frequency) => -frequency }.take(n)
   }
 }
 
